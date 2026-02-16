2cd69f5513dfedb2bd776879bdabcb2e40713f2e
Daniel Ribeirinha-Braga <dmrbraga@gmail.com>
2026-02-16 10:02:59 -0700

    Added MyFram.sol.

---
+++ solidity_102/day_1/MyFarm.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract MyFram { 
+  // Reward paramaters.
+  addresss public lockedToken;
+  addresss public rewardToken;
+
+  //  Track how much is deposited.
+  mapping(address => uint) public timeOfDeposit;
+  mapping(address => uint) public amountDeposit;
+
+  // Create instane of ERC20 tokens.
+  address public depositAddress;
+  address public rewardAddress;
+
+  constructor() {
+
+  }
+
+  function deposit(uint amount) external {
+    IERC20(depositAddress).transferFrom(msg.sender, address(this), amount);
+    timeOfDeposit[msg.sender] = block.timestamp;
+    amountDeposit[msg.sender] += amountToDeposit;
+  }
+
+  function claim() external {
+
+  }
+
+  function claim() external {
+
+  }
+}
